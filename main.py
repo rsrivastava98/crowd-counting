bf90da99fbf9fa166aaba5ed7d868e4f518cf1e0
Karina Bao <karina_bao@brown.edu>
2020-04-22 00:41:03 -0400

    density maps generated for parts a and b for test and train

--- main.py
+++ main.py
@@ -5,28 +5,39 @@ from scipy import misc
 import os
 from tensorflow.keras.preprocessing import image
 import preprocessing
+import sys
+from joblib import Parallel, delayed
+
+
+__DATASET_ROOT = "data/shanghaitech_h5_empty/ShanghaiTech/"
+
+def density_wrapper(img_path, output_path):
+    imgfile = image.load_img(img_path)
+    mat_file = img_path.replace('.jpg', '.mat').replace('images', 'ground-truth').replace('IMG_', 'GT_IMG_')
+    img = image.img_to_array(imgfile)
+    points = sco.loadmat(mat_file)
+    arr = points['image_info'][0][0][0][0][0]
+    print(img_path, mat_file)
+    preprocessing.generate_density_map(img, arr, img_path, output_path)
 
 
 def main():
-    train_images_path = "data/ShanghaiTech/part_A/train_data/"
-    test_images_path = "data/ShanghaiTech/part_A/test_data/"
+    a_train, a_test, b_train, b_test = preprocessing.generate_shanghaitech_path(__DATASET_ROOT)
 
-    img_file_list = []
-    
-    for root, _, files in os.walk(train_images_path):
-        for name in files:
-            if name.endswith(".jpg"):
-                img_file_list.append(os.path.join(root, name))
+    ### READ THIS! So there's a chance your computer will overheat, so I recommend uncommenting
+    # one block at a time (leave the other 3 blocks commented out) because it will take about 20-30 minutes/block...
 
+    output_name = "ShanghaiTech_PartA_Train/"
+    Parallel(n_jobs=4)(delayed(density_wrapper)(i) for i in a_train)
 
-    for i in range(1):
-        imgfile = image.load_img(img_file_list[i])
-        mat_file = img_file_list[i].replace('.jpg', '.mat').replace('images', 'ground-truth').replace('IMG_', 'GT_IMG_')
-        img = image.img_to_array(imgfile)
-        points = sco.loadmat(mat_file)
-        arr = points['image_info'][0][0][0][0][0]
-        print(img_file_list[i], mat_file)
-        preprocessing.generate_density_map(img, arr)
+    # output_name = "ShanghaiTech_PartA_Test/"
+    # Parallel(n_jobs=4)(delayed(density_wrapper)(i) for i in a_test)
+
+    # output_name = "ShanghaiTech_PartB_Train/"
+    # Parallel(n_jobs=4)(delayed(density_wrapper)(i, output_name) for i in b_train)
+
+    # output_name = "ShanghaiTech_PartB_Test/"
+    # Parallel(n_jobs=4)(delayed(density_wrapper)(i) for i in b_test)
 
 
 
--- preprocessing.py
+++ preprocessing.py
@@ -9,10 +9,45 @@ from scipy.io import loadmat
 import glob
 import h5py
 import time
-from sklearn.externals.joblib import Parallel, delayed
+# from sklearn.externals.joblib import Parallel, delayed
 import sys
 from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
+
+__DATASET_ROOT = "data/shanghaitech_h5_empty/ShanghaiTech/"
+# __OUTPUT_NAME = "ShanghaiTech_PartB_Test/"
+
+def generate_shanghaitech_path(root):
+    # now generate the ShanghaiA's ground truth
+    part_A_train = os.path.join(root, 'part_A/train_data', 'images')
+    part_A_test = os.path.join(root, 'part_A/test_data', 'images')
+    part_B_train = os.path.join(root, 'part_B/train_data', 'images')
+    part_B_test = os.path.join(root, 'part_B/test_data', 'images')
+
+    img_paths_a_train, img_paths_a_test, img_paths_b_train, img_paths_b_test = [], [], [], []
+
+    for roots, _, files in os.walk(part_A_train):
+        for name in files:
+            if name.endswith(".jpg"):
+                img_paths_a_train.append(os.path.join(roots, name))
+
+    for roots, _, files in os.walk(part_A_test):
+        for name in files:
+            if name.endswith(".jpg"):
+                img_paths_a_test.append(os.path.join(roots, name))
+
+    for roots, _, files in os.walk(part_B_train):
+        for name in files:
+            if name.endswith(".jpg"):
+                img_paths_b_train.append(os.path.join(roots, name))
+
+    for roots, _, files in os.walk(part_B_test):
+        for name in files:
+            if name.endswith(".jpg"):
+                img_paths_b_test.append(os.path.join(roots, name))
+
+    return img_paths_a_train, img_paths_a_test, img_paths_b_train, img_paths_b_test
+
 
 def gaussian_filter_density(gt):
     density = np.zeros(gt.shape, dtype=np.float32)
@@ -26,7 +61,7 @@ def gaussian_filter_density(gt):
     neigh.fit(pts)
     distances = neigh.kneighbors(pts)[0]
 
-    print('generate density...')
+    # print('generate density...')
     for i, pt in enumerate(pts):
         pt2d = np.zeros(gt.shape, dtype=np.float32)
         pt2d[pt[1], pt[0]] = 1.
@@ -36,19 +71,24 @@ def gaussian_filter_density(gt):
             sigma = np.average(np.array(gt.shape)) / 2. / 2.  # case: 1 point
 
         density += scipy.ndimage.filters.gaussian_filter(pt2d, sigma, mode='constant')
-    print('done.')
+    # print('done.')
     return density
 
-def generate_density_map(img, gt):
-
+def generate_density_map(img, gt, img_path, h5_path):
+    
     gt_loc = np.zeros((img.shape[0], img.shape[1]))
     for i in range(0, len(gt)):
         if int(gt[i][1]) < img.shape[0] and int(gt[i][0]) < img.shape[1]:
             gt_loc[int(gt[i][1]), int(gt[i][0])] = 1
     gt_loc = gaussian_filter_density(gt_loc)
 
-    io.imshow(gt_loc)
-    plt.show()
+    output_path = img_path.replace(__DATASET_ROOT, h5_path).replace('.jpg', '.h5').replace('images','ground-truth-h5')
+    output_dir = os.path.dirname(output_path)
+    os.makedirs(output_dir, exist_ok=True)
+    print("output", output_path)
+
+    # io.imshow(gt_loc)
+    # plt.show()
     sys.stdout.flush()
-    with h5py.File("image_density", 'w') as hf:
+    with h5py.File(output_path, 'w') as hf:
         hf['density'] = gt_loc
