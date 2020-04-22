dc4c46d08b9055eb48a608426292556bb03351fa
Karina Bao <karina_bao@brown.edu>
2020-04-21 22:59:30 -0400

    pulling

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import numpy as np
-from skimage import io 
+from skimage import io
 import scipy.io as sco
 from scipy import misc
 import os
+from tensorflow.keras.preprocessing import image
+import preprocessing
+
 
 def main():
     train_images_path = "data/ShanghaiTech/part_A/train_data/"
     test_images_path = "data/ShanghaiTech/part_A/test_data/"
 
     img_file_list = []
-    ground_truth_list = []
-
+    
     for root, _, files in os.walk(train_images_path):
-        for name in files:   
+        for name in files:
             if name.endswith(".jpg"):
                 img_file_list.append(os.path.join(root, name))
-            if name.endswith(".mat"):
-                ground_truth_list.append(os.path.join(root, name))
-      
-    
-    for img_path in img_file_list:
-        img = io.imread(img_path)
 
-    for ground_truth in ground_truth_list:
-        points = sco.loadmat(ground_truth)
+
+    for i in range(1):
+        imgfile = image.load_img(img_file_list[i])
+        mat_file = img_file_list[i].replace('.jpg', '.mat').replace('images', 'ground-truth').replace('IMG_', 'GT_IMG_')
+        img = image.img_to_array(imgfile)
+        points = sco.loadmat(mat_file)
         arr = points['image_info'][0][0][0][0][0]
-        print(arr)
+        print(img_file_list[i], mat_file)
+        preprocessing.generate_density_map(img, arr)
 
-        
+
 
 if __name__ == '__main__':
     main()
-
-
