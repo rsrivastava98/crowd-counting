b1331afbca86099e337b56c506e89c1c08b0e429
Rudra Srivastava <rudrasrivastava@Rudras-MacBook-Pro.local>
2020-05-05 02:49:51 +0530

    differential

--- differential.py
+++ differential.py
@@ -36,13 +36,13 @@ def differential_train(train_data, networks):
             y_pc = np.argmin(net_losses)
             model = networks[y_pc]
 
-            x = tf.constant(im)
+            x = tf.constant(image)
             with tf.GradientTape() as tape:
                 tape.watch(x)
-                loss = model.loss(model.call(x), dens)
+                loss = model.loss(model.call(im), dens)
                 #loss = np.abs(np.sum(model.call(im)) - np.sum(density))
 
-            grads = tape.gradient(loss, model.trainable_weights)
+            grads = tape.gradient(loss, x)
 
             model.optimizer.apply_gradients(zip(grads, model.trainable_weights))
 
